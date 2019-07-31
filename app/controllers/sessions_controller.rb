9e6878d7f049c07be9b8c38be1a5739009d7600d
Ashish Rao <ashishrao2598@gmail.com>
2019-07-31 16:10:17 +0530

    add logout and login methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  include CurrentUseConcern
+
   def create
     user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
 
@@ -12,5 +14,26 @@ class SessionsController < ApplicationController
     else
       render json: { status: 401 }
     end
+  end
+
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: {
+      status: 200,
+      logged_out: true
+    }
   end
 end
