dead6ce71c5a19f3ea665e07eb0d93fa6c23d7c6
Ashish Rao <ashishrao2598@gmail.com>
2019-08-05 11:54:59 +0530

    add session specs and update controller code

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,26 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe 'POST /sessions' do
-  describe 'valid scenario' do
-    it 'logs in a user' do
-      User.create(email: 'abc@gmail.com', password: 'password')
-      post '/sessions', params: {
-        'email' => 'abc@gmail.com',
-        'password' => 'password'
-      }
-      json = JSON.parse(response.body)
-      expect(json['logged_in']).to be_truthy
-    end
+describe 'valid scenario' do
+  before do
+    User.create(email: 'abc@gmail.com', password: 'password')
+    post '/sessions', params: {
+      'email' => 'abc@gmail.com',
+      'password' => 'password'
+    }
+  end
+  it 'logs in a user' do
+    json = JSON.parse(response.body)
+    expect(json['logged_in']).to be_truthy
   end
 
-  describe 'invalid scenario' do
-    it 'receives 422 status code' do
-      post '/sessions', params: {
-        'email' => 'abc@gmail.com',
-        'password' => 'password'
-      }
-      json = JSON.parse(response.body)
-      expect(json['status']).to eq(422)
-    end
+  it 'checks if user is logged in' do
+    get '/logged_in'
+    json = JSON.parse(response.body)
+    expect(json['user']).to eq('abc@gmail.com')
+  end
+end
+
+describe 'invalid scenario' do
+  it 'receives 422 status code' do
+    post '/sessions', params: {
+      'email' => 'abc@gmail.com',
+      'password' => 'password'
+    }
+    json = JSON.parse(response.body)
+    expect(json['status']).to eq(422)
   end
 end
